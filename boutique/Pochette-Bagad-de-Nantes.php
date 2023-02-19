f172d99f876fdb43a50d1b3b5ade7f98cd2a2bbc
Ronan Guilloux <ronan.guilloux@gmail.com>
2023-02-20 00:20:07 +0100

    index marges + Pochettes

---
+++ boutique/Pochette-Bagad-de-Nantes.php
@@ -0,0 +1,48 @@
+<?php
+require_once('../parts/default_metas.php');
+
+$meta['title'] = "Bagad de Nantes - Boutique";
+//$meta['description'] = "";
+
+require_once('../parts/head.html.php');
+require_once('../parts/navbar.html.php');
+?>
+<div class="container">
+  <div class="row row-cols-1 row-cols-md-2 pt-5">
+
+    <div class="col-md-8">
+      <h1 class="mt-0 d-print-none">Boutique</h1>
+      <hr class="d-print-none"/>
+      <img class="rounded float-end img-fluid img-thumbnail rounded mx-auto d-block w-25 d-none d-print-block" 
+          src="/boutique/assets/qr-code-boutique.png" />
+      <h2>Pochette à fermeture éclair Bagad de Nantes <span id="price" class="badge rounded-pill bg-light text-dark">26,00 €</span></h2>
+      <p><i>Prix unitaire maximum - varie en fonction du nombre d'unités commandées</i></p>
+      <?php require_once('parts/order.html.php'); ?>
+      <img class="rounded float-start img-fluid img-thumbnail rounded mx-auto d-block" 
+          src="/boutique/assets/packshots/Pochette-Bagad-de-Nantes.jpeg" />
+      <p>Pochette à fermeture éclair officielle du Bagad de Nantes</p>
+      <ul>
+        <li>Polyester résistant</li>
+        <li>Fermeture éclair intégrée</li>
+      </ul>
+      <p><b>Couleur</b> : Noir</p>
+      <p><b>Marquage</b> : Impression couleur</p>
+      <p><b>Format</b> : 31 x 21 cm.</p>
+      <h3>Un rangement unique en son genre</h3>
+      <p>Ce petit sac en polyester résistant et à fermeture éclair peut servir de trousse à accessoires, à maquillage,
+        de trousse de voyage, ou de rangement pour votre papeterie ou les objets essentiels à votre quotidien.
+        Pourquoi ne pas les offrir à un anniversaire ou à Noël ?</p>
+    </div>
+    <?php require_once('../parts/sidebar.html.php'); ?>
+
+  </div><!-- /row -->
+</div><!-- /container -->
+
+<?php require_once('../parts/cards.html.php'); ?>
+
+<?php require_once('../parts/footer.html.php'); ?>
+
+<?php require_once('../parts/scripts.html.php'); ?>
+
+</body>
+</html>
--- boutique/index.php
+++ boutique/index.php
@@ -16,52 +16,56 @@ require_once('../parts/navbar.html.php');
       <hr/>
 
       <a href="/boutique/Polo-Femme-Bagad-de-Nantes">  
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail" 
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
           src="/boutique/assets/packshots/Polo-Femme-Bagad-de-Nantes.jpeg" /></a>
 
       <a href="/boutique/Polo-Homme-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
           src="/boutique/assets/packshots/Polo-Homme-Bagad-de-Nantes.jpeg" /></a>
       
       <a href="/boutique/Sweat-shirt-zippe-a-capuche-Femme-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
           src="/boutique/assets/packshots/Sweat-shirt-zippe-a-capuche-Femme-Bagad-de-Nantes.jpeg" /></a>
       
       <a href="/boutique/Sweat-shirt-zippe-a-capuche-Homme-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
           src="/boutique/assets/packshots/Sweat-shirt-zippe-a-capuche-Homme-Bagad-de-Nantes.jpeg" /></a>
       
       <a href="/boutique/Polaire-Unisexe-col-zippe-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"
           src="/boutique/assets/packshots/Polaire-Unisexe-col-zippe-Bagad-de-Nantes.jpeg" /></a>
 
       <a href="/boutique/Veste-Softshell-Femme-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
           src="/boutique/assets/packshots/Veste-Softshell-Femme-Bagad-de-Nantes.jpeg" /></a>
 
       <a href="/boutique/Veste-Softshell-Homme-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+       <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
           src="/boutique/assets/packshots/Veste-Softshell-Homme-Bagad-de-Nantes.jpeg" /></a>
 
       <a href="/boutique/T-Shirt-Femme-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"   
           src="/boutique/assets/packshots/T-Shirt-Femme-Bagad-de-Nantes.jpeg" /></a>
 
       <a href="/boutique/T-Shirt-Homme-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"   
           src="/boutique/assets/packshots/T-Shirt-Homme-Bagad-de-Nantes.jpeg" /></a>
 
       <a href="/boutique/Sacoche-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
           src="/boutique/assets/packshots/Sacoche-Bagad-de-Nantes.jpeg" /></a>
 
       <a href="/boutique/Grand-sac-fourre-tout-coton-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
           src="/boutique/assets/packshots/Grand-sac-fourre-tout-coton-Bagad-de-Nantes.jpeg" /></a>
 
       <a href="/boutique/Sac-de-voyage-week-end-Bagad-de-Nantes">
-        <img width="200px" class="rounded float me-2 mb-2 img-fluid img-thumbnail"  
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"   
           src="/boutique/assets/packshots/Sac-de-voyage-week-end-Bagad-de-Nantes.jpeg" /></a>
+
+      <a href="/boutique/Pochette-Bagad-de-Nantes">
+        <img class="w-25 rounded float img-fluid img-thumbnail me-1 mb-2"  
+          src="/boutique/assets/packshots/Pochette-Bagad-de-Nantes.jpeg" /></a>
     </div>
     <?php require_once('../parts/sidebar.html.php'); ?>
 
