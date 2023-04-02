dca8ac505b379cabf435bd7ad0cb099039a121d1
Ronan Guilloux <ronan.guilloux@gmail.com>
2023-04-02 17:30:36 +0200

    partners

--- nos-partenaires.php
+++ nos-partenaires.php
@@ -17,17 +17,29 @@ require_once('parts/navbar.html.php');
         En 2022, le Bagad de Nantes ouvre son programme de Partenariats.
       </p>
       <h2 class="mt-5">Nos Partenaires</h2>
+
       <img class="img-fluid shadow-sm p-1 my-5 bg-body rounded"
       src="/assets/images/logos/logo-ville-de-nantes.webp" alt="Mairie de Nantes" />
       <p>La <a href="">Mairie de Nantes</a>, dont le Bagad porte le nom, nous accorde une subvention annuelle de 2.000 EUR.</p>
 
       <hr class="mt-5"/>
+
       <img class="img-fluid shadow-sm my-5 p-1 bg-body rounded"
       src="/assets/images/logos/logo-ccoc2.png" alt="Cercle Celtique Olivier de Clisson" />
       <p>
-        Depuis plusieurs années, le <a href="http://www.cercledeclisson.fr/" target="_blank">
+        Depuis plusieurs années, le <a href="http://www.cercledeclisson.fr" target="_blank">
           Cercle Celtique Olivier de Clisson</a> accompagne le Bagad de Nantes lors de sorties ou de prestations communes.
-        </p>
+      </p>
+      
+        <hr class="mt-5"/>
+      
+      <img class="img-fluid shadow-sm w-50 my-5 p-1 bg-body rounded"
+      src="/assets/images/logos/logo-lab.png" alt="Little Attlantique Brewery" />
+      <p>
+        La brasserie <a href="https://little-atlantique-brewery.fr" target="_blank">Little Atlantique Brewery</a>
+         accueille le Bagad de Nantes pour ses répétitions publiques annuelles, ainsi que pour des prestations publiques.  
+      </p>
+
       <hr class="mt-5"/>
     </div>
 
