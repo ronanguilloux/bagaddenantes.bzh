46b1bc89ba9c2616af7e2030d6b6aec4dae6c8b1
Ronan Guilloux <ronan.guilloux@gmail.com>
2023-04-15 10:19:54 +0200

    photos

---
+++ photos.php
@@ -0,0 +1,115 @@
+<?php
+	require_once('parts/default_metas.php');
+
+	$meta['title'] = "Bagad de Nantes - Vidéos";
+	//$meta['description'] = "";
+
+	require_once('parts/head.html.php');
+	require_once('parts/navbar.html.php');
+?>
+	<div class="container">
+		<div class="row row-cols-1 row-cols-md-2 pt-5">
+
+			<div class="col-md-8">
+				<h1 class="mt-0">Photos</h1>
+
+				<hr/>
+
+				<h2>2023</h2>
+
+				<h3>Saint-Brieuc - Concours de Printemps 2023</h2>
+
+				<p>Crédits : <a 
+					href="https://www.flickr.com/photos/yoannlegoff" 
+					target="_blank">Yoann Le Goff</a></p>
+				<p><a href="https://www.flickr.com/groups/bagadnaoned/pool/yoannlegoff" 
+					target="_blank"><img class="img-fluid shadow-sm bg-body rounded"
+      		src="/assets/photos/2023-YoannLeGoff-StBrieuc.png"
+					alt="Yoann Le Goff" /></a></p>
+
+				<p>Crédits : <a href="https://www.facebook.com/media/set/?vanity=alysouuu&set=a.3335874866676501"
+					target="blank">Alysée Piriou</a></p>
+				<p><a href="https://drive.google.com/drive/folders/1yeGLNZ3AsgJNF7E_aHfM7Xlv1Mcj2los?usp=sharing"
+					target="_blank"><img class="img-fluid shadow-sm bg-body rounded"
+      		src="/assets/photos/2023-AlyseePiriou-StBrieuc.png"
+					alt="Alysée Piriou" /></a></p>
+				
+				<hr/>
+
+				<h2>2022</h2>
+				
+				<h3>Lorient - Championnat des Bagadoù 2022</h2>
+				<p>Crédits : <a href="https://www.facebook.com/media/set/?set=a.10220258476964865"
+						target="_blank">Fous De-folk</a>
+				<a href="https://drive.google.com/drive/folders/1Dep6PxBEekcFG_gY_1zJKi4BUKF8MwOB?usp=sharing" 
+					target="_blank"><img class="img-fluid shadow-sm bg-body rounded"
+      		src="/assets/photos/2022-Fous-De-folk-Lorient.png"
+					alt="Fous De-folk" /></a>
+					
+				<hr />
+				
+				<h3>Pont-L'Abbé - Triomphe des Sonneurs - Festival des Brodeuses 2022</h2>
+				<p>Crédits : <a href="https://www.facebook.com/media/set/?set=a.465546478908763"
+						target="_blank">Denis Prigent</a>
+				<a href="https://drive.google.com/drive/folders/1ZD27n0jEdekp22CvDWKBH5W_2ZsWs078?usp=sharing" 
+					target="_blank"><img class="img-fluid shadow-sm bg-body rounded"
+      		src="/assets/photos/2022-DenisPrigent-Brodeuses.png"
+					alt="Denis Prigent" /></a>		
+				
+				<hr />
+
+				<h3>Nantes - Triomphe des Sonneurs - Jeux de Bretagne 2022</h2>
+				<p>Crédits : <a href="https://www.instagram.com/alieno_leso/" target="_blank">@alieno_leso</a></p>
+				<a href="https://photos.app.goo.gl/Uq7hx78TofsjpCtFA" 
+					target="_blank"><img class="img-fluid shadow-sm bg-body rounded"
+      		src="/assets/photos/2022-alieno_leso-TriompheJeuxDeBretagne.png"
+					alt="@alieno_leso" /></a>
+
+				<hr />
+
+				<h3>Nantes - Ouverture des Jeux de Bretagne 2022</h2>
+				<p>Crédits : <a href="https://www.instagram.com/alieno_leso/" target="_blank">@alieno_leso</a></p>
+				<a href="https://photos.app.goo.gl/psaASgSFm3k4jXSr5" 
+					target="_blank"><img class="img-fluid shadow-sm bg-body rounded"
+      		src="/assets/photos/2022-alieno_leso-OuvertureJeuxDeBretagne.png"
+					alt="@alieno_leso" /></a>
+				
+				<hr />
+
+				<h3>Saint-Brieuc - Concours de Printemps 2022</h2>
+				<p>Crédits : <a 
+					href="https://www.mathieu-leguern.fr" 
+					target="_blank">Mathieu Le Guern - Krank Du / Design & PhotoGraphisme</a></p>
+				<a href="https://drive.google.com/drive/folders/14EijqMdvdnOflCdjMiUsB9rDBIpkLMcZ?usp=sharing" 
+					target="_blank"><img class="img-fluid shadow-sm bg-body rounded"
+      		src="/assets/photos/2022-KrankDu-StBrieuc.png"
+					alt="Mathieu Le Guern - Krank Du / Design & PhotoGraphisme" /></a>
+				
+				<hr />
+
+				<h2>2021</h2>
+
+				<h3>Clisson - Celtomania 2021</h3>
+				<p>Crédits : Kristell Mimoun</p>
+				<a href="https://drive.google.com/drive/folders/1mrcaNl3WFgOAdjkzSlQo6E-AByQU9_G2?usp=sharing" 
+					target="_blank"><img class="img-fluid shadow-sm bg-body rounded"
+      		src="/assets/photos/2021-KristellMimoun-Celtomania.png"
+					alt="Mathieu Le Guern - Krank Du / Design & PhotoGraphisme" /></a>
+					
+				<hr />	
+
+			</div>
+
+			<?php require_once('parts/sidebar.html.php'); ?>
+
+		</div><!-- /row -->
+	</div><!-- /container -->
+
+<?php require_once('parts/cards.html.php'); ?>
+
+<?php require_once('parts/footer.html.php'); ?>
+
+<?php require_once('parts/scripts.html.php'); ?>
+
+</body>
+</html>
